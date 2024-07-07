d901acc3d27deb7c61556023769987fc4e9e32d8
Andrew gathu <gathuandrew7@gmail.com>
2024-07-07 10:27:28 +0300

    Add arithmetic operations script

---
+++ fns_and_dsa/arithmetic_operations.py
@@ -0,0 +1,22 @@
+def perform_operation(num1, num2, operation):
+    """
+    Perform arithmetic operation based on the given inputs.
+    
+    :param num1: First number (float)
+    :param num2: Second number (float)
+    :param operation: String specifying the operation ('add', 'subtract', 'multiply', 'divide')
+    :return: Result of the operation or an error message
+    """
+    if operation == 'add':
+        return num1 + num2
+    elif operation == 'subtract':
+        return num1 - num2
+    elif operation == 'multiply':
+        return num1 * num2
+    elif operation == 'divide':
+        if num2 != 0:
+            return num1 / num2
+        else:
+            return "Error: Division by zero"
+    else:
+        return "Invalid operation"
